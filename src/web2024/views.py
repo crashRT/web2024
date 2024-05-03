333a33445ef17ef7d6902adc01bb0fdd3966b282
crashRT <crashrt.farbauti@gmail.com>
2024-05-03 12:45:55 +0900

    profile表示部分

--- src/web2024/views.py
+++ src/web2024/views.py
@@ -5,3 +5,7 @@ from web2024 import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/profile/<string:id>')
+def profile(id):
+    return render_template('profile/'+id+'/index.html')
